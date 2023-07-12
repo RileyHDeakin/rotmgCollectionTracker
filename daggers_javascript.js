092a45f98117bf86f2f3e295d380b0aefa9e55d8
RileyHDeakin <109202681+RileyHDeakin@users.noreply.github.com>
2023-07-12 15:31:30 +1000

    Add files via upload

---
+++ daggers_javascript.js
@@ -0,0 +1,80 @@
+function check_dagger_T1(){
+  var element = document.getElementById("1");
+  element.style.opacity = "0.2";
+}
+
+function check_dagger_T2(){
+  var element = document.getElementById("2");
+  element.style.opacity = "0.2";
+}
+
+function check_dagger_T3(){
+  var element = document.getElementById("3");
+  element.style.opacity = "0.2";
+}
+
+function check_dagger_T4(){
+  var element = document.getElementById("4");
+  element.style.opacity = "0.2";
+}
+
+function check_dagger_T5(){
+  var element = document.getElementById("5");
+  element.style.opacity = "0.2";
+}
+
+function check_dagger_T6(){
+  var element = document.getElementById("6");
+  element.style.opacity = "0.2";
+}
+
+function check_dagger_T7(){
+  var element = document.getElementById("7");
+  element.style.opacity = "0.2";
+}
+
+function check_dagger_T8(){
+  var element = document.getElementById("8");
+  element.style.opacity = "0.2";
+}
+
+function check_dagger_T9(){
+  var element = document.getElementById("9");
+  element.style.opacity = "0.2";
+}
+
+function check_dagger_T10(){
+  var element = document.getElementById("10");
+  element.style.opacity = "0.2";
+}
+
+function check_dagger_T11(){
+  var element = document.getElementById("11");
+  element.style.opacity = "0.2";
+}
+
+function check_dagger_T12(){
+  var element = document.getElementById("12");
+  element.style.opacity = "0.2";
+}
+
+function check_dagger_T13(){
+  var element = document.getElementById("13");
+  element.style.opacity = "0.2";
+}
+
+function check_dagger_T14(){
+  var element = document.getElementById("14");
+  element.style.opacity = "0.2";
+}
+
+
+function check_dagger_T14(){
+  var element = document.getElementById("14");
+  element.style.opacity = "0.2";
+}
+
+function check_bow_T0(){
+  var element = document.getElementById("00");
+  element.style.opacity = "0.2";
+}
